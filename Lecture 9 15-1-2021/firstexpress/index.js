d32e8916f0dc128927223ffa34ab76786f59246a
Usman Akram <usman.akram@gmail.com>
2021-01-15 11:04:44 +0500

    express

--- Lecture 9 15-1-2021/firstexpress/index.js
+++ Lecture 9 15-1-2021/firstexpress/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 app.use(express.json()); //middleware
-
+app.use(express.static("public")); // serve files from public folder
 app.set("view engine", "pug"); //setting pug as view engine
 
 app.get("/about", (req, res) => {
   res.render("about");
+});
+app.get("/contactus", (req, res) => {
+  res.render("contactus");
 });
 app.get("/", (req, res) => {
   res.render("homepage");
